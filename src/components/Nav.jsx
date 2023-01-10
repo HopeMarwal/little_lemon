8e139eae8cae56bcbfb208b3f9abc4dd74987962
Cascaval Nadejda <vremea.dr@gmail.com>
2023-01-10 10:46:27 +0000

    Meta tags

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,11 +4,20 @@ import Logo from '../assets/img/Logo.svg'
 export default function Nav() {
   return (
     <nav>
+
       <div className="logo">
-        <a href='/'>
-          <img src={Logo} alt="little_lemon" />
-        </a>
+        <img src={Logo} alt="little_lemon" />
       </div>
+
+      <ul>
+        <li><a href='/'>home</a></li>
+        <li><a href='/about'>about</a></li>
+        <li><a href='/menu'>menu</a></li>
+        <li><a href='/reservation'>reservation</a></li>
+        <li><a href='/order'>order online</a></li>
+        <li><a href='/login'>login</a></li>
+      </ul>
+
     </nav>
   )
 }
